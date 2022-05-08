0913133ba7069e766414aee9c8053224da5dab6c
Archy <archy.revera@protonmail.com>
2022-05-08 15:21:47 +0300

    Update opencv code: add class configurations

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/cvstd.hpp>
 #include <opencv2/videostab/inpainting.hpp>
+#include <opencv2/videostab/stabilizer.hpp>
+#include <opencv2/videostab/optical_flow.hpp>
+#include <opencv2/videostab/motion_stabilizing.hpp>
+
+namespace vs = cv::videostab;
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -10,32 +17,36 @@ int main(int argc, char** argv) {
 
     std::string video_path {argv[1]};
 
-    // open video from file
-    cv::VideoCapture cap(video_path);
-    if (!cap.isOpened()) {
-        std::cerr << "Error opening video stream or file" << std::endl;
-        return -1;
-    }
+    // VideoFileSource
+    cv::Ptr<vs::VideoFileSource> source_ptr = cv::makePtr<vs::VideoFileSource>(video_path);
 
-    cv::Mat frame;
-    // iterate through video frames
-    while (true) {
-        cap >> frame;
-        if (frame.empty())
-            break;
+    vs::TwoPassStabilizer two_pass_stab;
 
-        // inpainting
-        cv::videostab::MotionInpainter inpainter;
-        cv::Mat mask;
-        inpainter.inpaint(1, frame, mask);
+    // Choose and configure motion stabilizer
+    vs::GaussianMotionFilter motion_filter{3 /* ??? */};
+    cv::Ptr<vs::GaussianMotionFilter> motion_stab = cv::makePtr<vs::GaussianMotionFilter>(motion_filter);
+    two_pass_stab.setMotionStabilizer(motion_stab);
 
-        // show result
-        cv::imshow("frame", frame);
-        cv::imshow("mask", mask);
-        if (cv::waitKey(30) >= 0) {
-            break;
-        }
-    }
+    // Do we also need to set motion estimator?
+
+    two_pass_stab.setFrameSource(source_ptr);
+
+
+    vs::InpaintingPipeline *inpainters = new vs::InpaintingPipeline();
+
+    cv::Ptr<vs::ConsistentMosaicInpainter> mosaic_inp = cv::makePtr<vs::ConsistentMosaicInpainter>();
+    mosaic_inp->setStdevThresh(3 /* ??? */);
+    inpainters->pushBack(mosaic_inp);
+
+    cv::Ptr<vs::MotionInpainter> motion_inp = cv::makePtr<vs::MotionInpainter>();
+    motion_inp->setDistThreshold(3 /* ??? */);
+    inpainters->pushBack(motion_inp);
+
+    inpainters->setRadius(3 /* ??? */);
+    two_pass_stab.setInpainter(inpainters);
+
+    // Iterate through frames and do stabilization
+    // TODO: implement
 
     return 0;
 }
