1da01db97d8f1f37f490078cb9961b2011ffaa18
Archy <archy.revera@protonmail.com>
2022-05-24 12:53:23 +0300

    Add wobble suppressor, use adapted optical flow & minor updates all around

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+/*
+ * Some stabilization pipeline ideas taken from: 
+ *   https://github.com/didpurwanto/video-stabilization-using-openCV
+ */
 #include <iostream>
 
 #include <opencv2/opencv.hpp>
@@ -5,6 +9,8 @@
 #include <opencv2/videostab/inpainting.hpp>
 #include <opencv2/videostab/stabilizer.hpp>
 #include <opencv2/videostab/motion_stabilizing.hpp>
+
+#include "adapted_optical_flow.hpp"
 
 namespace vs = cv::videostab;
 
@@ -15,39 +21,57 @@ int main(int argc, char** argv) {
     }
 
     std::string output_path = "output.mp4";
-    double output_fps;
-
     std::string video_path {argv[1]};
 
     // VideoFileSource
     cv::Ptr<vs::VideoFileSource> source_ptr = cv::makePtr<vs::VideoFileSource>(video_path);
-
-    output_fps = source_ptr->fps();
+    double output_fps = source_ptr->fps();
 
     vs::TwoPassStabilizer two_pass_stab;
     two_pass_stab.setFrameSource(source_ptr);
 
+
+    two_pass_stab.setEstimateTrimRatio(true);
+    two_pass_stab.setTrimRatio(0.1);
+    two_pass_stab.setCorrectionForInclusion(true);
+    two_pass_stab.setBorderMode(cv::BORDER_REPLICATE);
+
+
     // Choose and configure motion stabilizer
-    vs::GaussianMotionFilter motion_filter{3 /* ??? */};
+    vs::GaussianMotionFilter motion_filter{15, -1.0 /* ??? */};
     cv::Ptr<vs::GaussianMotionFilter> motion_stab = cv::makePtr<vs::GaussianMotionFilter>(motion_filter);
     two_pass_stab.setMotionStabilizer(motion_stab);
 
-    // Do we also need to set motion estimator?
+
+    // Wobble suppressor
+    cv::Ptr<vs::MoreAccurateMotionWobbleSuppressorBase> wobble_sup = cv::makePtr<vs::MoreAccurateMotionWobbleSuppressor>();
+    wobble_sup = cv::makePtr<vs::MoreAccurateMotionWobbleSuppressorGpu>();
+    wobble_sup->setPeriod(30 /* ??? */);
+    two_pass_stab.setWobbleSuppressor(wobble_sup);
+
 
     // Configure inpainters
     vs::InpaintingPipeline *inpainters = new vs::InpaintingPipeline();
+    cv::Ptr<vs::InpainterBase> inpainters_base(inpainters);
 
+    // Mosaicing
     cv::Ptr<vs::ConsistentMosaicInpainter> mosaic_inp = cv::makePtr<vs::ConsistentMosaicInpainter>();
-    mosaic_inp->setStdevThresh(3 /* ??? */);
+    mosaic_inp->setStdevThresh(10.0 /* ??? */);
     inpainters->pushBack(mosaic_inp);
 
-    cv::Ptr<vs::MotionInpainter> motion_inp = cv::makePtr<vs::MotionInpainter>();
-    motion_inp->setDistThreshold(3 /* ??? */);
-    inpainters->pushBack(motion_inp);
 
-    inpainters->setRadius(3 /* ??? */);
-    two_pass_stab.setInpainter(inpainters);
+    // Motion Inapinter
+    cv::Ptr<vs::MotionInpainter> inp = cv::makePtr<vs::MotionInpainter>();
+    inp->setDistThreshold(5.0 /* ??? */);
 
+    cv::Ptr<vs::IDenseOptFlowEstimator> flowEstimator;
+    flowEstimator = cv::makePtr<Adapted_DensePyrLkOptFlowEstimatorGpu>();
+    inp->setOptFlowEstimator(flowEstimator);
+
+    inpainters->pushBack(inp);
+
+    inpainters->setRadius(15 /* ??? */);
+    two_pass_stab.setInpainter(inpainters_base);
 
 
     cv::VideoWriter writer;
@@ -55,26 +79,15 @@ int main(int argc, char** argv) {
     int nframes = 0;
     char file_name[100];
 
-    // for each stabilized frame
+    // Iterate through frameso, stabilize them and write to new video file
     while ( !(stabilizedFrame = two_pass_stab.nextFrame()).empty() ) {
         nframes++;
 
         // init writer (once) and save stabilized frame
         if (!writer.isOpened())
-            writer.open(output_path, cv::VideoWriter::fourcc('X','V','I','D'), output_fps, stabilizedFrame.size());
+            writer.open(output_path, cv::VideoWriter::fourcc('X','V','I','D'),
+                        output_fps, stabilizedFrame.size());
         writer << stabilizedFrame;
-
-        // show stabilized frame
-        cv::imshow("stabilizedFrame", stabilizedFrame);
-        sprintf(file_name, "%0.3d.tif", nframes);
-
-        cv::imwrite(file_name, stabilizedFrame);
-
-        char key = static_cast<char>(cv::waitKey(3));
-        if (key == 27) {
-            std::cout << std::endl;
-            break;
-        }
     }
 
     return 0;
